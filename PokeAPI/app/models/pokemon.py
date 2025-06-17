a2eaf73134c619865d417cf4dd0d9c31531be8e7
Seth Lifland <lifland37@gmail.com>
2025-06-16 22:17:12 -0700

    Add models for pokemon and move, fetch user-provided pokemon and its first move

---
+++ PokeAPI/app/models/pokemon.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel, Field
+from typing import List, Annotated, Optional
+
+class VersionGroupRef(BaseModel):
+    name: str
+    url: str
+
+class MoveVersionGroupDetails(BaseModel):
+    level_learned_at: int
+    order: Optional[int] = None  # Allow null/missing values
+    version_group: VersionGroupRef
+
+class MoveRef(BaseModel):
+    name: str
+    url: str
+
+class MoveRefWrapper(BaseModel):
+    move: MoveRef
+    version_group_details: List[MoveVersionGroupDetails]
+
+class Pokemon(BaseModel):
+    id: int
+    name: str
+    base_experience: int
+    height: int
+    weight: int
+    moves: List[MoveRefWrapper]
+
+class Move(BaseModel):
+    id: int
+    name: str
+    accuracy: Annotated[int, Field(ge=0, le=100)]
+    pp: int
+    priority: int
+    power: int
--- PokeAPI/app/pokeapi.py
+++ PokeAPI/app/pokeapi.py
@@ -4,6 +4,8 @@ from urllib.parse import urljoin
 from types import MappingProxyType
 from dataclasses import dataclass
 from typing import Final, Callable
+import argparse
+from models.pokemon import Pokemon, Move
 
 load_dotenv()
 
@@ -12,11 +14,9 @@ BASE_URL: Final = "https://pokeapi.co/api/v2/"
 @dataclass(frozen=True)
 class Endpoints:
     pokemon: Callable[[str], str]
-    moves: Callable[[str], str]
 
 ENDPOINTS: Final = Endpoints(
     pokemon=lambda name: f"pokemon/{name}",
-    moves=lambda name: f"move/{name}"
 )
 
 def get_pokemon(name):
@@ -25,7 +25,23 @@ def get_pokemon(name):
     response = requests.get(
         url=url
     )
-    return response
+    response_dict = response.json()
+    pokemon = Pokemon(**response_dict)
+    print(f"Fetched pokemon: {pokemon.name}, ({pokemon.id:03})")
+    print(f"Getting move...")
+    get_move(pokemon)
+
+def get_move(pokemon: Pokemon):
+    url = pokemon.moves[0].move.url
+    print(f"move_url: {url}")
+    response = requests.get(url=url)
+    move = Move(**response.json())
+    print(f"Fetched move: {move.name} ({move.id})")
 
 if __name__ == "__main__":
-    get_pokemon("bulbasaur")
+    parser = argparse.ArgumentParser("Get a pokemon, along with a list of their moves")
+    parser.add_argument("name", help="Provide the name of a pokemon")
+
+    args = parser.parse_args()
+    name = args.name
+    get_pokemon(name)
